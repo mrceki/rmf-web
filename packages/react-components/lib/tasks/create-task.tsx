c1cf4107abf2032d23a546e16fa43b36600ac065
gurgenufuk12 <doguhangurgen8gmail.com>
2024-01-05 13:20:39 +0000

    fix: create-task form dark theme color fix

--- packages/react-components/lib/tasks/create-task.tsx
+++ packages/react-components/lib/tasks/create-task.tsx
@@ -135,10 +135,10 @@ const StyledDialog = styled((props: DialogProps) => <Dialog {...props} />)(({ th
   [`& .${classes.actionBtn}`]: {
     minWidth: 80,
     borderRadius: '20px',
-    backgroundColor: theme.palette.mode === 'dark' ? '#CE172D' : '#CE172D',
+    backgroundColor: theme.palette.mode === 'dark' ? '#739BD0' : '#CE172D',
     '&:hover': {
       transform: 'scale(1.05)',
-      backgroundColor: theme.palette.mode === 'dark' ? '#CE172D' : '#CE172D',
+      backgroundColor: theme.palette.mode === 'dark' ? '#739BD0' : '#CE172D',
     },
   },
   [`& .${classes.createTaskForm}`]: {
