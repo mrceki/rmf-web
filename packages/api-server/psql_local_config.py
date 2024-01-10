bc49b6c86bc9e163137f1c6a7808384f9fd7b053
gurgenufuk12 <doguhangurgen8gmail.com>
2024-01-10 08:44:36 +0000

    feat: db_url change with db type

--- packages/api-server/psql_local_config.py
+++ packages/api-server/psql_local_config.py
@@ -1,11 +1,20 @@
-from sqlite_local_config import config
+from os.path import dirname
+
+from api_server.default_config import config
 
 here = dirname(__file__)
 run_dir = f"{here}/run"
 
+# Do not forget to change the username and password depending on your postgresql configuration
+# The default username and password is ugurgen
+# The default port is 5432
+# The default database name is rmf
+# The default host is 127.0.0.1
+# If you want to change the default configuration, you can change it in the following line
+
 config.update(
     {
-        "db_url": "postgres://postgres:postgres@127.0.0.1:5432",
+        "db_url": "postgres://ugurgen:ugurgen@127.0.0.1:5432/rmf",  # change ugurgen depending on your username and password after port/{db_name}
         "cache_directory": f"{run_dir}/cache",  # The directory where cached files should be stored.
     }
 )
